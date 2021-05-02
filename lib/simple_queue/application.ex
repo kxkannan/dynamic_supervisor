518816f6bf541cd6f4a4101a521984bb1e37f477
kannan <kxkannan@gmail.com>
2021-05-02 16:50:16 -0600

    Initial commit of dynamic supervisor functionality in the queue_manager

---
+++ lib/simple_queue/application.ex
@@ -0,0 +1,21 @@
+defmodule SimpleQueue.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [{QueueManager, []}]
+    opts = [strategy: :one_for_one, name: QueueManager.Supervisor]
+
+    options = [
+      name: SimpleQueue.Supervisor,
+      strategy: :one_for_one
+    ]
+
+    DynamicSupervisor.start_link(options)
+    Supervisor.start_link(children, opts)
+  end
+end
---
+++ lib/simple_queue/queue_manager.ex
@@ -0,0 +1,68 @@
+defmodule QueueManager do
+  use GenServer
+
+  require Logger
+
+  def start_link(state \\ %{}) do
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
+
+  def start_children(num) do
+    GenServer.call(__MODULE__, {:start_children, num})
+  end
+
+  def list_children() do
+    GenServer.call(__MODULE__, :list_children)
+  end
+
+  def stop_child(pid) do
+    GenServer.call(__MODULE__, {:stop_child, pid})
+  end
+
+  @impl GenServer
+  def init(args) do
+    state = %{children: args}
+    {:ok, state}
+  end
+
+  @impl GenServer
+  def handle_call({:start_children, num}, _, state) do
+    child_info =
+      Enum.reduce(1..num, %{}, fn n, acc ->
+        child_name = "SimpleQueue" <> Integer.to_string(n)
+
+        {:ok, pid} =
+          DynamicSupervisor.start_child(SimpleQueue.Supervisor, %{
+            id: "SimpleQueue" <> Integer.to_string(n),
+            start: {SimpleQueue, :start_link, [String.to_atom(child_name), [n, n + 1, n + 2]]}
+          })
+
+        acc = Map.put(acc, child_name, pid)
+        acc
+      end)
+
+    state = %{state | children: child_info}
+
+    {:reply, state.children, state}
+  end
+
+  def handle_call(:list_children, _, state) do
+    {:reply, state.children, state}
+  end
+
+  def handle_call({:stop_child, name}, _, state) do
+    IO.inspect(name, label: "Stopping child name: ")
+
+    case Map.has_key?(state.children, name) do
+      true ->
+        pid = state.children[name]
+        response = DynamicSupervisor.terminate_child(SimpleQueue.Supervisor, pid)
+        state = %{state | children: Map.delete(state.children, name)}
+        {:reply, response, state}
+
+      false ->
+        Logger.error("Child with the name #{name} not found")
+        {:reply, {:error, :not_found}, state}
+    end
+  end
+end
